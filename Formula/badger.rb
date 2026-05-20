0499a4bd7daabec2769a84162de9040eff9a5f6d
ejboy <kupolov@gmail.com>
2026-05-20 09:54:25 -0700

    brew: bump formula to v0.1.4

--- Formula/badger.rb
+++ Formula/badger.rb
@@ -1,30 +1,30 @@
 class Badger < Formula
   desc "Local context bridge for bringing codebase context to an AI chat"
   homepage "https://github.com/PVRLabs/aibadger"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/PVRLabs/aibadger/releases/download/v#{version}/badger_#{version}_darwin_arm64.tar.gz"
-      sha256 "96d2581664d3b8b9c4393b0469427165fb25afb3287ce3ab42c0acb1725822eb"
+      sha256 "5d039e457a312a2df8ba6da95065253ceb06086bb8f47e6a5e6d4564cf487bf7"
     end
 
     on_intel do
       url "https://github.com/PVRLabs/aibadger/releases/download/v#{version}/badger_#{version}_darwin_amd64.tar.gz"
-      sha256 "2f39cb09c17f70dca522811f25c32d42abd8c4877cf1d5a83dc714bc29c73949"
+      sha256 "1727a35e680ab0461095d9913914320ce6eb60beb6bca3f5f2dd4cc508017c80"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/PVRLabs/aibadger/releases/download/v#{version}/badger_#{version}_linux_arm64.tar.gz"
-      sha256 "67cf5a9996ed81f6079e0d9f7237be2ec58fa1409307f8e4f408ae5bddce70df"
+      sha256 "fb992b94e80ee44d9bf7bbd2de6749ac377cb955614469046f2dab92d02698f4"
     end
 
     on_intel do
       url "https://github.com/PVRLabs/aibadger/releases/download/v#{version}/badger_#{version}_linux_amd64.tar.gz"
-      sha256 "2c1515c838fa3215e2435d3c41efcadfe36159ece54f917ad42e7942b4ec680a"
+      sha256 "f0abc15e4bbac81e37b4b0dc17ac10e57c21adc4accb6d016846acdcaf1c6e55"
     end
   end
 
